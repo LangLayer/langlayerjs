b4f52dc8125f9d5efd33257cc42e97c1e1aa0ffe
Karim lahlou <eteflamingo@gmail.com>
2025-08-02 10:48:27 -0400

    Update package.js

--- package.js
+++ package.js
@@ -1,5 +1,45 @@
 {
-  "name": "langlayer",
+  "name": "@langlayer/langlayer",
   "version": "1.0.0",
-  "author": "Karim Lahlou <karimlahlou@gmail.com> (https://langlayer.com)"
+  "description": "Composable tools for building LLM-powered apps",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/LangLayer/langlayerjs.git"
+  },
+  "keywords": [
+    "llm",
+    "language-model",
+    "large-language-model",
+    "ai",
+    "artificial-intelligence",
+    "machine-learning",
+    "nlp",
+    "natural-language-processing",
+    "prompt-engineering",
+    "langchain",
+    "langlayer",
+    "composability",
+    "modular",
+    "workflow",
+    "pipeline",
+    "ai-toolkit",
+    "nodejs",
+    "javascript",
+    "python-integration",
+    "vector-search",
+    "embedding",
+    "api",
+    "sdk"
+  ],
+  "author": "Karim Lahlou",
+  "license": "MIT",
+  "bugs": {
+    "url": "https://github.com/LangLayer/langlayerjs/issues"
+  },
+  "homepage": "https://github.com/LangLayer/langlayerjs#readme",
+  "type": "commonjs"
 }
